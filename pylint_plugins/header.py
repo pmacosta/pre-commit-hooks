512bbcce986ea5ef6c9432256df9c203f014eb47
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-01-27 11:01:11 -0500

    Take a template as a header reference

--- pylint_plugins/header.py
+++ pylint_plugins/header.py
@@ -4,6 +4,7 @@
 # pylint: disable=C0111
 
 # Standard library imports
+from __future__ import print_function
 import datetime
 import os
 import re
@@ -15,59 +16,85 @@ from pylint.checkers import BaseChecker
 
 
 ###
+# Global variables
+###
+IS_PY3 = sys.hexversion > 0x03000000
+
+
+###
 # Functions
 ###
-def _tostr(line):
-    if isinstance(line, str):
-        return line
-    if sys.hexversion > 0x03000000:
-        return line.decode()
-    return line.encode()
+def _find_header_ref(fname):
+    """Find .headerrc file."""
+    curr_dir = ""
+    next_dir = os.path.dirname(os.path.abspath(fname))
+    while next_dir != curr_dir:
+        curr_dir = next_dir
+        rcfile = os.path.join(curr_dir, ".headerrc")
+        if os.path.exists(rcfile):
+            return rcfile
+        next_dir = os.path.dirname(curr_dir)
+    return ""
 
 
-def check_header(node, comment="#"):
+def _read_file(fname):
+    """Return file lines as strings."""
+    with open(fname) as fobj:
+        for line in fobj:
+            yield _tostr(line).strip()
+
+
+def _tostr(obj):  # pragma: no cover
+    """Convert to string if necessary."""
+    return obj if isinstance(obj, str) else (obj.decode() if IS_PY3 else obj.encode())
+
+
+def check_header(node, comment="#", header_ref=""):
     """Check that all files have header line and copyright notice."""
     # pylint: disable=W0702
+    header_ref = header_ref.strip() or _find_header_ref(node.file)
+    if not header_ref:
+        print(
+            "Reference header file .headerrc not found, skipping header check",
+            file=sys.stderr,
+        )
+        return []
+    fullname = os.path.basename(os.path.abspath(node.file))
     basename = os.path.basename(os.path.abspath(node.file))
-    year = datetime.datetime.now().year
-    header_lines = [
-        re.compile("{0} {1}".format(comment, basename)),
-        re.compile(
-            r"{0} Copyright \(c\) (?:{1}|(\d+)-{1}) "
-            "Pablo Acosta-Serafini".format(comment, year)
-        ),
-        re.compile("{0} See LICENSE for details".format(comment)),
-    ]
+    current_year = datetime.datetime.now().year
+    header_lines = []
+    for line in _read_file(header_ref):
+        header_lines.append(
+            line.format(
+                comment=comment,
+                fullname=fullname,
+                basename=basename,
+                current_year=current_year,
+            )
+        )
     linenos = []
     with node.stream() as stream:
         for (num, line), ref in zip(content_lines(stream, comment), header_lines):
-            match = ref.match(line)
-            if not match:
+            if num > len(header_lines):
+                break
+            if line != ref:
                 linenos.append(num)
-            else:
-                groups = match.groups()
-                if groups and groups[0]:
-                    try:
-                        if int(groups[0]) >= year:
-                            linenos.append(num)
-                    except:
-                        linenos.append(num)
     return linenos
 
 
 def content_lines(stream, comment="#"):
     """Return non-empty lines of a package."""
-    skip_lines = ["#!/bin/bash", "#!/usr/bin/env bash", "#!/usr/bin/env python"]
+    shebang_line_regexp = re.compile(r"^#!.*[ \\/](bash|python)$")
     encoding_dribble = "\xef\xbb\xbf"
     encoded = False
-    cregexp = re.compile(r".*{0} -\*- coding: utf-8 -\*-\s*".format(comment))
+    cregexp = re.compile(r"^{0} -\*- coding: utf-8 -\*-\s*".format(comment))
     for num, line in enumerate(stream):
         line = _tostr(line).rstrip()
         if (not num) and line.startswith(encoding_dribble):
             line = line[len(encoding_dribble) :]
         coding_line = (num == 0) and (cregexp.match(line) is not None)
         encoded = coding_line if not encoded else encoded
-        shebang_line = (num == int(encoded)) and (line in skip_lines)
+        shebang_line = (num == int(encoded)) and shebang_line_regexp.match(line)
         if line and (not coding_line) and (not shebang_line):
             yield num + 1, line
 
@@ -98,14 +125,25 @@ class HeaderChecker(BaseChecker):
             ),
         )
     }
-    options = ()
+
+    options = (
+        (
+            "header-ref",
+            {
+                "default": "",
+                "type": "string",
+                "metavar": "<header reference>",
+                "help": "Header reference",
+            },
+        ),
+    )
 
     def process_module(self, node):
         """Process a module. Content is accessible via node.stream() function."""
-        linenos = check_header(node)
-        if linenos:
-            for lineno in linenos:
-                self.add_message(self.NON_COMPLIANT_HEADER, line=lineno)
+        # pylint: disable=E1101
+        linenos = check_header(node, header_ref=self.config.header_ref)
+        for lineno in linenos:
+            self.add_message(self.NON_COMPLIANT_HEADER, line=lineno)
 
 
 def register(linter):
