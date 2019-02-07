369fa1720b153f64a6cf04786fa135bc6fca0c3a
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-02-07 11:58:28 -0500

    Switched to hunspell for easier cross-platform support

--- pre_commit_hooks/spelling.py
+++ pre_commit_hooks/spelling.py
@@ -2,7 +2,7 @@
 # spelling.py
 # Copyright (c) 2019 Pablo Acosta-Serafini
 # See LICENSE for details
-# pylint: disable=C0103,C0111,R0912,R0914
+# pylint: disable=C0103,C0111,R0912,R0914,R1718
 
 # Standard library imports
 from __future__ import print_function
@@ -84,16 +84,37 @@ IS_PY3 = sys.hexversion > 0x03000000
 ###
 # Functions
 ###
+def _cleanup_word(word):
+    """Strip out leading trailing spaces, quotes and double quotes."""
+    if not word.strip():
+        return ""
+    new_word = word.strip().strip('"').strip().strip("'").strip()
+    new_word = new_word.strip('"').strip().strip("'")
+    while new_word != word:
+        word = new_word
+        new_word = word.strip().strip('"').strip().strip("'").strip()
+        new_word = new_word.strip('"').strip().strip("'")
+    return new_word
+
+
 def _grep(fname, words):
     """Return line numbers in which words appear in a file."""
     # pylint: disable=W0631
     pat = "(.*[^a-zA-Z]|^){}([^a-zA-Z].*|$)"
-    regexps = zip(words, [re.compile(pat.format(word)) for word in words])
+    regexps = [(word, re.compile(pat.format(word))) for word in words]
     ldict = collections.defaultdict(list)
     for num, line in enumerate(_read_file(fname)):
         for word in [word for word, regexp in regexps if regexp.match(line)]:
             ldict[word].append(str(num + 1))
     return ldict
+
+
+def _make_abspath(value):
+    """Homogenize files to have absolute paths."""
+    value = value.strip()
+    if not os.path.isabs(value):
+        value = os.path.abspath(os.path.join(os.getcwd(), value))
+    return value
 
 
 def _read_file(fname):
@@ -110,8 +131,7 @@ def _tostr(obj):  # pragma: no cover
 
 def _valid_file(value):
     """Check that a file exists and returned it converted to absolute path."""
-    if not os.path.isabs(value):
-        value = os.path.abspath(os.path.join(os.getcwd(), value))
+    value = _make_abspath(value)
     if not os.path.exists(value):
         raise argparse.ArgumentTypeError("File {0} does not exist".format(value))
     return value
@@ -119,35 +139,45 @@ def _valid_file(value):
 
 def check_spelling(argv=None):
     """Run aspell and report line number in which misspelled words are."""
-    if not which("aspell"):
-        print("aspell not found, skipping spell checking")
+    if not which("hunspell"):
+        print("hunspell not found, skipping spell checking")
         return 0
     argv = sys.argv[1:] if argv is None else argv
     # Apparently the personal dictionary cannot be a relative path
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--personal", nargs=1, type=_valid_file, required=False)
+    parser.add_argument("-d", nargs=1, required=False)
+    parser.add_argument("-i", nargs=1, required=False)
+    parser.add_argument("-p", nargs=1, type=_valid_file, required=False)
+    parser.add_argument("-P", nargs=1, required=False)
     parser.add_argument("-e", "--exclude", nargs=1, type=_valid_file, required=False)
     parser.add_argument("files", nargs="*", type=_valid_file)
     cli_args, cmd_args = parser.parse_known_args(argv)
     ###
-    if cli_args.personal:
-        cmd_args.append("--personal={}".format(cli_args.personal[0]))
     fnames = cli_args.files
     if cli_args.exclude:
-        patterns = list(_read_file(cli_args.exclude[0]))
+        patterns = [_make_abspath(item) for item in _read_file(cli_args.exclude[0])]
         exclude_filter = lambda x: not any(fnmatch(x, pattern) for pattern in patterns)
         fnames = filter(exclude_filter, fnames)
     ###
-    cmd = ["aspell"] + cmd_args + ["list"]
+    cmd_args += ["-d", cli_args.d[0]] if cli_args.d else []
+    cmd_args += ["-i", cli_args.i[0]] if cli_args.i else []
+    cmd_args += ["-p", cli_args.p[0]] if cli_args.p else []
+    cmd_args += ["-P", cli_args.P[0]] if cli_args.P else []
     retval = 0
+    base_cmd = ["hunspell"] + cmd_args + ["-l"]
     for fname in fnames:
-        with io.open(fname) as fobj:
-            obj = Popen(cmd, stdin=fobj, stdout=PIPE, stderr=PIPE)
-            stdout = _tostr(obj.communicate()[0]).split(os.linesep)
-        words = sorted(list(set([line for line in stdout if line.strip()])))
+        cmd = base_cmd + [fname]
+        obj = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        stdout = _tostr(obj.communicate()[0]).split(os.linesep)
+        words = [_cleanup_word(word) for word in stdout if word.strip()]
+        words = sorted(list(set([word for word in words if word])))
+        header_printed = False
         if words:
             retval = 1
             ldict = _grep(fname, words)
+            if not header_printed:
+                print("Base command: "+(" ".join(base_cmd)))
+                header_printed = True
             print(fname)
             for word, lines in [(word, ldict[word]) for word in words]:
                 plural = "s" if len(lines) > 1 else ""
